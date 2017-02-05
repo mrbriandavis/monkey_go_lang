37ca3d87fffea7a66098fc8447d64916ea180664
Brian Davis <bdavis@yammer-inc.com>
2017-02-05 13:01:53 -0800

    more ast improvements, basic parser to handle let statements and parser tests

--- ast/ast.go
+++ ast/ast.go
@@ -1,6 +1,6 @@
 package ast
 
-import "mokey/token"
+import "monkey/token"
 
 type Node interface {
 	TokenLiteral() string
@@ -20,6 +20,14 @@ type Program struct {
 	Statements []Statement
 }
 
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
 type LetStatement struct {
 	Token token.Token // the token.LET token
 	Name *Identifier
@@ -29,15 +37,9 @@ type LetStatement struct {
 func (ls *LetStatement) statementNode()			{}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+type Identifier struct {
+	Token token.Token // the token.IDENT token 
+	Value string
+}
 func (i *Identifier) expressionNode() 			{}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
-
-
-
-func (p *Program) TokenLiteral() string {
-	if len(p.Statements) > 0 {
-		return p.Statements[0].TokenLiteral()
-	} else {
-		return ""
-	}
-}
